2512ada49d31aca81d4d51201e4037d78341a4da
NeoRain314 <jasmin.dammertz@googlemail.com>
2025-09-05 17:44:43 +0200

    multiplayer moving synchronized

--- jumpandrun_multiplayer/scripts/multiplayer/multiplayer_controller.gd
+++ jumpandrun_multiplayer/scripts/multiplayer/multiplayer_controller.gd
@@ -1,29 +1,68 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 50.0
+const JUMP_VELOCITY = -210.0
+var player_number: int = 1
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var direction = 1
 
 
 @export var player_id := 1:
 	set(id):
 		player_id = id
+		%InputSynchronizer.set_multiplayer_authority(id)
 
-func _physics_process(delta: float) -> void:
+func _apply_animations(delta):
+	# Flip the Sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	# Play animation
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("p" + str(player_number) + "_idle")
+		else:
+			animated_sprite.play("p" + str(player_number) + "_run")
+	else:
+		animated_sprite.play("p" + str(player_number) + "_jump")
+
+func _apply_movement_from_input(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("p" + str(player_number) + "_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get the input direction: -1, 0, 1
+	var direction = %InputSynchronizer.input_direction
+	
+	
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	#move box
+	var collision = move_and_collide(velocity * delta)
+	
+	if collision:
+		if collision.get_collider() is CharacterBody2D:
+			var box = collision.get_collider()
+			var push_force = velocity.x / 1.5
+			box.velocity.x = push_force
+	
+	#move character
 	move_and_slide()
+
+
+func _physics_process(delta: float) -> void:
+	if multiplayer.is_server():
+		_apply_movement_from_input(delta)
---
+++ jumpandrun_multiplayer/scripts/multiplayer/multiplayer_input.gd
@@ -0,0 +1,18 @@
+extends MultiplayerSynchronizer
+
+var input_direction
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	if get_multiplayer_authority() != multiplayer.get_unique_id():
+		set_process(false)
+		set_physics_process(false)
+	input_direction = Input.get_axis("p" + str(1) + "_move_left", "p" + str(1) + "_move_right")
+
+func _physics_process(delta: float) -> void:
+	input_direction = Input.get_axis("p" + str(1) + "_move_left", "p" + str(1) + "_move_right")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
