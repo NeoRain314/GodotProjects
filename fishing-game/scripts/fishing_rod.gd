fff3cdb54dd64c86d65c69ab0c3068e25b2223e5
NeoRain314 <jasmin.dammertz@googlemail.com>
2026-08-05 07:57:13 +0200

    added shaking to fishing rod and shadow to boat

--- fishing-game/scripts/fishing_rod.gd
+++ fishing-game/scripts/fishing_rod.gd
@@ -1,22 +1,36 @@
 extends Node2D
 
 var counter : int = 0
+var _time: float
+var shake_speed: int = 20
+var max_shake: float = 0.5
+var start_position_x: float
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$smallSplash.visible = false
+	$splash.visible = false
+	start_position_x = $rod.position.x
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	_time += delta
+	
 	if Input.is_action_just_pressed("ui_left"):
+		#$rod.position.x = start_position_x
 		counter += 1
 		if counter > 2: counter = 0
-	if counter == 0: $AnimatedSprite2D.play("idle")
-	if counter == 1: $AnimatedSprite2D.play("fishing")
-	if counter == 2: $AnimatedSprite2D.play("catch")
-	if randi() % 50 == 0 && counter != 0:
-		$smallSplash.visible = true
-		$smallSplash.play("default") #play different when catch animation or match position!!!!
-		print("splash")
-	if !$smallSplash.is_playing(): $smallSplash.visible = false
+		if counter == 0: $rod.play("idle")
+		if counter == 1: $rod.play("fishing")
+		if counter == 2: $rod.play("catch")
+	
+	if randi() % 50 == 0 && counter == 1:
+		$splash.visible = true
+		$splash.play("default")
+	if counter == 2:
+		$splash.visible = true
+		$splash.play("default") #play different animation or match position!!!!
+		$rod.position.x = start_position_x + sin(_time * shake_speed) * max_shake
+	
+	if !$splash.is_playing(): $splash.visible = false
