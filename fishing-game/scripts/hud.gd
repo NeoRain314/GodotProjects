d2d5d92be04e72cf2b457e3c5eb7541e3bed9991
NeoRain314 <jasmin.dammertz@googlemail.com>
2026-08-10 16:41:09 +0200

    added open and clole + animation to inventory

--- fishing-game/scripts/hud.gd
+++ fishing-game/scripts/hud.gd
@@ -1,10 +1,18 @@
 extends Control
 
 @export var inv_row_scene: PackedScene
-@onready var container = $PanelContainer/MarginContainer/ScrollContainer/VBoxContainer
+@onready var inv_container = $Inventory/MarginContainer/ScrollContainer/VBoxContainer
+@onready var inv_panel =  $Inventory
+
+var inventory_open: bool
 
 
 func _ready() -> void:
+	inv_panel.visible = false
+	inventory_open = false
+	inv_panel.pivot_offset = Vector2(inv_panel.size.x, 0)
+	
+	
 	InventoryManagerAl.inventory_changed.connect(update_inventory_ui) #connect so signal from inv manager
 	update_inventory_ui()
 
@@ -13,14 +21,27 @@ func _process(delta: float) -> void:
 
 
 func update_inventory_ui():
-	for child in container.get_children():
+	for child in inv_container.get_children():
 		child.queue_free()
 	for item_id in InventoryManagerAl.contents:
 		var amount = InventoryManagerAl.contents[item_id]
 		
 		var row_instance = inv_row_scene.instantiate()
-		container.add_child(row_instance)
+		inv_container.add_child(row_instance)
 		row_instance.setup(item_id, amount)
+
+func open_inventory():
+	inv_panel.visible = true
+	var tween = inv_panel.create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	inv_panel.scale = Vector2.ZERO
+	tween.tween_property(inv_panel, "scale", Vector2.ONE, 0.4)
+	inventory_open = true
+
+func close_inventory():
+	var tween = inv_panel.create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
+	tween.tween_property(inv_panel, "scale", Vector2.ZERO, 0.3)
+	tween.tween_callback(inv_panel.hide)
+	inventory_open = false
 
 # --- old ---
 #func update_inventory_ui():
@@ -28,3 +49,8 @@ func update_inventory_ui():
 	#for item_id in InventoryManagerAl.contents:
 		#inv_text += "- " + item_id + ": " + str(InventoryManagerAl.contents[item_id]) + "\n"
 	#$TempInventory.text = inv_text
+
+
+func _on_inv_button_pressed() -> void:
+	if inventory_open: close_inventory()
+	else: open_inventory()
