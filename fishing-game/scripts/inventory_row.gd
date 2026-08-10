1d92d416002be49dd562be38bb964f51e949d36c
NeoRain314 <jasmin.dammertz@googlemail.com>
2026-08-10 10:09:28 +0200

    first inventory visual changes

--- fishing-game/scripts/inventory_row.gd
+++ fishing-game/scripts/inventory_row.gd
@@ -1,12 +1,12 @@
-extends HBoxContainer
+extends PanelContainer
 
-@onready var item_icon = $TextureRect
-@onready var item_name = $NameLabel
-@onready var item_amount = $AmountLabel
+@onready var item_icon = $HBoxContainer/TextureRect
+@onready var item_name = $HBoxContainer/NameLabel
+@onready var item_amount = $HBoxContainer/AmountLabel
 
 func setup(item_id: String, amount: int):
 	var item = ItemManagerAl.get_item(item_id)
 	if item == null: push_warning("Item not fouond: " + item_id)
 	item_icon.texture = item.texture
 	item_name.text = item.name
-	item_amount = "x" + str(amount)
+	item_amount.text = "x" + str(amount)
