92db1616558f32044b94d8016cafea1d279a11bf
NeoRain314 <jasmin.dammertz@googlemail.com>
2026-08-11 12:23:41 +0200

    fixed cursor problem (now isnt centered and is correctly alined with real mouse)

--- fishing-game/scripts/autoload/GameManagerAL.gd
+++ fishing-game/scripts/autoload/GameManagerAL.gd
@@ -4,9 +4,9 @@ var curr_cursor_asset = 0
 var curr_cursor_text = ""
 
 #cursors
-var cursor_norm = load("res://assets/cursors/cursor_0.png")
-var cursor_select = load("res://assets/cursors/cursor_1.png")
-var cursor_click = load("res://assets/cursors/cursor_2.png")
+var cursor_norm = load("res://assets/UI/cursors/cursor_0.png")
+var cursor_select = load("res://assets/UI/cursors/cursor_1.png")
+var cursor_click = load("res://assets/UI/cursors/cursor_2.png")
 
 func _ready() -> void:
 	pass
